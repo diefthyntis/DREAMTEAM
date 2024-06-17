8a5ecd0537ed6495ddf6b71df918e9cbbe9a3839
Alan Hauser <diefthyntis@gmail.com>
2024-06-17 14:45:13 +0200

    version JO améliorée par André

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NgChartsModule } from 'ng2-charts';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule,NgChartsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
